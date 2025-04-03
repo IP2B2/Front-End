4aee8d946663100fb596aad84f23c8d806510722
beatrice~ <prodanbeatrice5@gmail.com>
2025-04-03 21:12:17 +0300

    form style done

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,22 +1,28 @@
 import Link from "next/link";
 import styles from "./loginPage.module.css";
 import '@/app/globals.css';
+import { InterHeading, InterText,  InterEmphasis} from '@/lib/fonts/Inter';
 
 export default function PasswordReset() {
 
-    return <div>
-        <div className={styles.title}> Autentificare </div>
-        <div className={styles.subtitle}>Introdu datele de autentificare în formularul de mai jos</div>
+    return <div className={styles.loginContainer}>
+        <div className={styles.title + " " + InterHeading.className}> Autentificare </div>
+        <div className={styles.subtitle + " " + InterText.className}>Introdu datele de autentificare în formularul de mai jos</div>
         
-        <form className={styles.form}>
+        <form className={styles.form + " " + InterEmphasis.className}>
             <div className={styles.formLabel}>Email</div>
-            <input type={"email"} placeholder={"exemplu@gmail.com"}></input>
+            <input type={"email"} className={"border-rounded" + " " + "border-gray"} placeholder={"exemplu@gmail.com"}></input>
             <div className={styles.formLabel}>Parola</div>
-            <input type={"password" + " " + "border-rounded"} placeholder={"*************"}></input>
-            <button type={"submit" + " " + "border-rounded"} className={styles.formButton}>Autentificare</button>
+            <input type={"password"} className={"border-rounded" + " " + "border-gray"} placeholder={"*************"}></input>
+            <button type={"submit"} className={styles.formButton + " " + "border-rounded"}>Autentificare</button>
         </form>
-        <Link href={"google.com"} className={styles.link + " " + "faded"} >Ai uitat parola?</Link>
-        <Link href={"google.com"} className={styles.link + " " + "faded"}>Nu ai un cont?</Link>
+        <div className={styles.linkWrapper + " " + InterText.className}>
+            <Link href={"google.com"} className={styles.link} >Ai uitat parola?</Link>
+        </div>
+        
+        <div className={styles.linkWrapper  + " " + InterText.className}> 
+            <Link href={"google.com"} className={styles.link}>Nu ai un cont?</Link>
+        </div>
 
     </div>;
 }
