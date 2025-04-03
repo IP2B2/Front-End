0287b58cb2555dfafda48c1eabd633303a0f875e
eejdan <ejdanprotonmail.com>
2025-04-03 22:39:47 +0300

    #003-login-page code split auth BannerContainer component

--- src/app/auth/layout.js
+++ src/app/auth/layout.js
@@ -1,9 +1,8 @@
 
 import '@/app/globals.css'
 import styles from './authLayout.module.css'
-import Image from 'next/image';
 
-import { Montserrat400 } from '@/lib/fonts/Montserrat';
+import BannerContainer from '@/lib/components/auth/BannerContainer'
 
 export default function AccountCreationLayout(props) {
     return <div className={styles.layoutContainer}>
@@ -11,17 +10,5 @@ export default function AccountCreationLayout(props) {
         <main>
             {props.children}
         </main>
-    </div>
-}
-
-const BannerContainer = () => {
-    return <div className={styles.bannerContainer}>
-        <Image
-            src="/ISMA.svg"
-            width={400}
-            height={152}
-            alt="Picture of the author"
-        />
-            <div className={styles.bannerAppTitle + ' ' + Montserrat400.className}> Smart access. Seamless research.</div>
     </div>
 }
---
+++ src/lib/components/auth/BannerContainer.js
@@ -0,0 +1,20 @@
+
+import Image from 'next/image';
+
+import { Montserrat400 } from '@/lib/fonts/Montserrat';
+
+import '@/app/globals.css'
+import styles from './BannerContainer.module.css'
+
+const BannerContainer = () => {
+    return <div className={styles.bannerContainer}>
+        <Image
+            src="/ISMA.svg"
+            width={400}
+            height={152}
+            alt="Picture of the author"
+        />
+            <div className={styles.bannerAppTitle + ' ' + Montserrat400.className}> Smart access. Seamless research.</div>
+    </div>
+}
+export default BannerContainer;
