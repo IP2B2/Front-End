adb7b42f76183dcb10c3754b66ad9eb736d828a0
beatrice~ <prodanbeatrice5@gmail.com>
2025-04-03 20:39:40 +0300

    firstcommit

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,6 +1,22 @@
-
+import Link from "next/link";
+import styles from "./loginPage.module.css";
+import '@/app/globals.css';
 
 export default function PasswordReset() {
 
-    return <div>login page -removethis</div>;
+    return <div>
+        <div className={styles.title}> Autentificare </div>
+        <div className={styles.subtitle}>Introdu datele de autentificare în formularul de mai jos</div>
+        
+        <form className={styles.form}>
+            <div className={styles.formLabel}>Email</div>
+            <input type={"email"} placeholder={"exemplu@gmail.com"}></input>
+            <div className={styles.formLabel}>Parola</div>
+            <input type={"password" + " " + "border-rounded"} placeholder={"*************"}></input>
+            <button type={"submit" + " " + "border-rounded"} className={styles.formButton}>Autentificare</button>
+        </form>
+        <Link href={"google.com"} className={styles.link + " " + "faded"} >Ai uitat parola?</Link>
+        <Link href={"google.com"} className={styles.link + " " + "faded"}>Nu ai un cont?</Link>
+
+    </div>;
 }
