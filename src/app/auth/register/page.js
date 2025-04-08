4547e2a35223903b64e7726ad9ddb3a9318cf2e4
Gacea Iasmina-Alecsia <gaceaiasmina@gmail.com>
2025-04-08 14:23:05 +0300

    #004_Creare-pagina-Register-First-Part

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -6,12 +6,17 @@ import Link from "next/link";
 function RegisterPage() {
   return (
     <div className={styles.formContainer}>
+      
+      {/* Titluri */}
       <h1 className={styles.title}>Creeaza cont</h1>
       <p className={styles.subtitle}>
         Completeaza formularul de mai jos pentru a crea un cont
       </p>
 
+      {/* Formular */}
       <form className={styles.form}>
+        
+        {/* Câmpul de Email */}
         <label className={styles.label}>
           Email
           <input
@@ -20,6 +25,8 @@ function RegisterPage() {
             className={styles.input}
           />
         </label>
+        
+        {/* Câmpul de Numar Matricol */}
         <label className={styles.label}>
           Numar matricol
           <input
@@ -27,10 +34,13 @@ function RegisterPage() {
             className={styles.input}
           />
         </label>
+
+        {/* Butonul de Submit */}
         <button type="submit" className={styles.button}>
           Creeaza cont
         </button>
 
+        {/* Link pentru utilizatori deja înregistrați */}
         <Link
           href="https://google.com"
           className={styles.loginHint}
