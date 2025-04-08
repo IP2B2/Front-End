2c52d44574e33deea3d74bc484a322d1deeba02c
Gacea Iasmina-Alecsia <gaceaiasmina@gmail.com>
2025-04-08 21:44:29 +0300

    #004-Register-page-changes-added

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -2,21 +2,24 @@
 
 import styles from './register.module.css';
 import Link from "next/link";
+import { InterText } from '@/lib/fonts/Inter';
+
+
 
 function RegisterPage() {
   return (
-    <div className={styles.formContainer}>
+    <div className={`${styles.formContainer} ${InterText.className}`}>
       
-      {/* Titluri */}
+      {}
       <h1 className={styles.title}>Creeaza cont</h1>
       <p className={styles.subtitle}>
         Completeaza formularul de mai jos pentru a crea un cont
       </p>
 
-      {/* Formular */}
+      {}
       <form className={styles.form}>
         
-        {/* Câmpul de Email */}
+        {}
         <label className={styles.label}>
           Email
           <input
@@ -26,7 +29,7 @@ function RegisterPage() {
           />
         </label>
         
-        {/* Câmpul de Numar Matricol */}
+        {}
         <label className={styles.label}>
           Numar matricol
           <input
@@ -35,12 +38,12 @@ function RegisterPage() {
           />
         </label>
 
-        {/* Butonul de Submit */}
+        {}
         <button type="submit" className={styles.button}>
           Creeaza cont
         </button>
 
-        {/* Link pentru utilizatori deja înregistrați */}
+        {}
         <Link
           href="https://google.com"
           className={styles.loginHint}
