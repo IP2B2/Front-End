eb2b46ca11c17719ca0b88ed588bfd7dcb31d32d
SergiuChelba <sergiu.mihai.chelba@gmail.com>
2025-04-22 15:41:17 +0300

    #028 implement confirm mail page structure

---
+++ src/app/auth/confirm-mail/page.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from "./loginPage.module.css";
+import '@/app/globals.css';
+import { Inter700, Inter500, Inter600 } from '@/lib/fonts/Inter';
+
+export default function PasswordReset() {
+
+    return <div className={styles.loginContainer}>
+        <div className={styles.formHeaderContainer}>
+            <div className={styles.title + " " + Inter700.className}> Autentificare </div>
+            <div className={styles.subtitle + " " + Inter500.className}>Introdu datele de autentificare în formularul de mai jos</div>
+        </div>
+
+        <form className={styles.form + " " + Inter600 .className}>
+            <div className={styles.formLabel}>Email</div>
+            <input type={"email"} className={"border-rounded" + " " + "border-gray"} placeholder={"exemplu@gmail.com"}></input>
+            <div className={styles.formLabel}>Parola</div>
+            <input type={"password"} className={"border-rounded" + " " + "border-gray"} placeholder={"*************"}></input>
+            <button type={"submit"} className={styles.formButton + " " + "border-rounded" + " " + Inter600 .className}>Autentificare</button>
+        </form>
+        
+        <div className={styles.linkWrapper + " " + Inter500.className}>
+            <Link href={"google.com"} className={styles.link} >Ai uitat parola?</Link>
+        </div>
+        
+        <div className={styles.linkWrapper  + " " + Inter500.className}> 
+            <Link href={"google.com"} className={styles.link}>Nu ai un cont?</Link>
+        </div>
+
+    </div>;
+}
