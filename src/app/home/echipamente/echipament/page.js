69f64387cd48f2279c410493dbbe98bf4b7f07d0
Gacea Iasmina-Alecsia <gaceaiasmina@gmail.com>
2025-05-06 17:05:40 +0300

    #035-Mockup-pagina-individuala-produs

--- src/app/home/echipamente/echipament/page.js
+++ src/app/home/echipamente/echipament/page.js
@@ -24,20 +24,57 @@ export default function EchipamentPage() {
         <div className={styles.container}>
             <div className={styles.content}>
             <div className={styles.layout}>
-                <div className={styles.imageContainer}>
-                    <img
-                        src="/icons/Frame 1000005448.svg"
-                        alt="Prelungitor Gri"
-                        className={styles.image}
+                <div className={styles.pageTitleContainer}>
+                    <img 
+                        src="/icons/back-arrow.svg" 
+                        alt="Back" 
+                        className={styles.backArrow}
                     />
                 </div>
+                <div className={styles.imageContainer}>
+                    <div className={styles.imageWrapper}>
+                        <img 
+                            src="/icons/Frame 1000005448.svg" 
+                            alt="Prelungitor Gri" 
+                            className={styles.image}
+                        />
+                        <div className={styles.overlay}>
+                            <span className={styles.overlayText}>FEEA</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.toggleContainer}>
+                    <button className={`${styles.toggleButton} ${styles.activeToggle}`}>
+                        Basic
+                    </button>
+                    <button className={`${styles.toggleButton} ${styles.disabledToggle}`} disabled>
+                        Complex
+                    </button>
+                </div>
+
                 <div className={styles.descriptionContainer}>
                     <div className={styles.pageDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ullamcorper diam, nec maximus lorem. Vivamus magna dolor, tristique vel elementum sit amet, dictum a dolor. Sed eget bibendum turpis. Nunc in mauris ut mauris pellentesque pulvinar. Integer efficitur vitae ligula ac egestas. Curabitur ultricies tincidunt erat dapibus porttitor. Pellentesque maximus aliquet elementum. Donec malesuada, enim at pulvinar interdum, nunc libero lacinia ante, id mattis mi sem ac neque. Cras ullamcorper, lacus sed sagittis consequat, velit odio rhoncus metus, vel blandit nisi leo et mi. Pellentesque a enim eu enim vestibulum ullamcorper non rutrum ex. Donec dolor ligula, gravida eget laoreet a, tempor at velit. Duis vel erat sed leo dignissim rutrum.
+                    Prelungitor gri cu 5 prize Schuko, cablu de 1.5 metri și întrerupător iluminat pentru control ușor. Suportă o putere maximă de 3680W și este ideal pentru utilizare casnică sau birou. Design compact, cu protecție la supratensiune integrată.
                     </div>
                     <div className={styles.buttonGroup}>
                         <button className={styles.actionButton}>Vezi disponibilitate</button>
                         <button className={styles.actionButton}>Inchiriaza</button>
+                    </div>
+                    <div className={styles.dropdownsContainer}>
+                        <details className={styles.dropdown}>
+                            <summary className={styles.dropdownHeader}>Mod de utilizare</summary>
+                            <div className={styles.dropdownContent}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ullamcorper diam, nec maximus lorem.
+                            </div>
+                        </details>
+                        
+                        <details className={styles.dropdown}>
+                            <summary className={styles.dropdownHeader}>Material si intretinere</summary>
+                            <div className={styles.dropdownContent}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ullamcorper diam, nec maximus lorem.
+                            </div>
+                        </details>
                     </div>
                 </div>
             </div>
