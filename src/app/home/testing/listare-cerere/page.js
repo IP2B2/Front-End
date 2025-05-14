c5d51ceea761f23a45776e0d08c2b166ad3c615f
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-14 17:21:20 +0300

    #066 - added onClick functionality

--- src/app/home/testing/listare-cerere/page.js
+++ src/app/home/testing/listare-cerere/page.js
@@ -5,8 +5,8 @@ import CerereListing from '@/lib/components/home/CerereListing';
 export default function Page() {
   return (
     <div className="content-wrapper">
-      <CerereListing title="Laptop Dell XPS 15" label="Mentenanță" />
-      <CerereListing title="Prelungitor cu mâner" label="Valabil"/>
+      <CerereListing title="Laptop Dell XPS 15" label="Mentenanță" onClick={()=> console.log("clicked - just checking if it works")} />
+      <CerereListing title="Prelungitor cu mâner" label="Valabil"onClick={()=> console.log("clicked - just testing again")}/>
     </div>
     
   );
--- src/lib/components/home/CerereListing.js
+++ src/lib/components/home/CerereListing.js
@@ -1,20 +1,23 @@
 import PropTypes from 'prop-types';
 import styles from './CerereListing.module.css';
 
-export default function CerereListing({ title, label }) {
+export default function CerereListing({ title, label, onClick }) {
 
   const isValabil = label === 'Valabil';
-  
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container}
+    onClick={onClick ? onClick : () =>{}}>
+
       <div className={styles.title}>{title}</div>
       <div className={isValabil ? styles.greenLabel : styles.redLabel} >{label}</div>
+
     </div>
   );
 }
 
-
 CerereListing.propTypes = {
   title: PropTypes.string.isRequired,
   label: PropTypes.oneOf(['Valabil', 'Mentenanță']).isRequired,
+  onClick: PropTypes.func,
 };
