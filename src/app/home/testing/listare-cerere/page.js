6f3770ced2dca138156a882f7f1470365cf8977b
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-14 17:06:24 +0300

    #066 - added label color change functionality

--- src/app/home/testing/listare-cerere/page.js
+++ src/app/home/testing/listare-cerere/page.js
@@ -5,7 +5,9 @@ import CerereListing from '@/lib/components/home/CerereListing';
 export default function Page() {
   return (
     <div className="content-wrapper">
-      <CerereListing title="Laptop Dell XPS 15" label="Valabil" />
+      <CerereListing title="Laptop Dell XPS 15" label="Mentenanță" />
+      <CerereListing title="Prelungitor cu mâner" label="Valabil"/>
     </div>
+    
   );
 }
--- src/lib/components/home/CerereListing.js
+++ src/lib/components/home/CerereListing.js
@@ -1,10 +1,20 @@
+import PropTypes from 'prop-types';
 import styles from './CerereListing.module.css';
 
 export default function CerereListing({ title, label }) {
+
+  const isValabil = label === 'Valabil';
+  
   return (
     <div className={styles.container}>
       <div className={styles.title}>{title}</div>
-      <div className={styles.label} >{label}</div>
+      <div className={isValabil ? styles.greenLabel : styles.redLabel} >{label}</div>
     </div>
   );
 }
+
+
+CerereListing.propTypes = {
+  title: PropTypes.string.isRequired,
+  label: PropTypes.oneOf(['Valabil', 'Mentenanță']).isRequired,
+};
