d03c7866f9a66ffdc9bd5afc3e102ae3af943ed0
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-15 16:16:15 +0300

    #086-Pagina confirmare decizie admin

---
+++ src/app/home/confirmare-decizie-admin/page.js
@@ -0,0 +1,28 @@
+'use client'
+
+import styles from './confirmDecisionAdmin.module.css';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.back(); //de modificat cand se implementeaza pagina de administrare utilizatori
+    }, 7000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.contentBox}>
+        <div className={styles.message}>Răspunsul dvs. a fost înregistrat</div>
+      </div>
+    </div>
+    
+  );
+}
