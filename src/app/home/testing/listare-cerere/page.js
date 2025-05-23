a269db3b72580b9a002dd432101a12531c8436b1
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-23 17:05:01 +0300

    #107-componenta-listare-cerere-student/cercetator

--- src/app/home/testing/listare-cerere/page.js
+++ src/app/home/testing/listare-cerere/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import CerereListing from '@/lib/components/home/CerereListing';
+import CerereListing from '@/lib/components/home/echipamente/CerereListingStudent';
 import CerereListingLaborant from '@/lib/components/home/echipamente/CerereListingLaborant';
 import ListareUser from '@/lib/components/home/echipamente/ListareUser';
 import ListareProdus from '@/lib/components/home/echipamente/ProdusListing';
@@ -8,26 +8,27 @@ import ListareProdus from '@/lib/components/home/echipamente/ProdusListing';
 export default function Page() {
   return (
     <div className="content-wrapper">
-      <p>Cereri listing studenti</p>
-      <CerereListing title="Laptop Dell XPS 15" label="Mentenanță" onClick={()=> console.log("clicked - just checking if it works")} />
-      <CerereListing title="Prelungitor cu mâner" label="Valabil"onClick={()=> console.log("clicked - just testing again")}/>
 
-      <p>Cereri listing laborant: </p>
-      <CerereListingLaborant title="Prelungitor 1" studentName="Prodan Beatrice" label="Pending"onClick={()=> console.log("clicked - just testing again")}/>
-      <CerereListingLaborant title="Prelungitor 2" studentName="Prodan Beatrice" label="Accepted"onClick={()=> console.log("clicked - just testing again")}/>
-      <CerereListingLaborant title="Prelungitor 3" studentName="Prodan Beatrice" label="Rejected"onClick={()=> console.log("clicked - just testing again")}/>
+      <p>Cereri studenti</p>
+      <CerereListing 
+        title="Laptop Dell XPS 15" 
+        label="Pending" 
+        location = "Facultatea de Informatica FII"
+        onClick={()=> console.log("clicked - just checking if it works")} />
 
-      <p>Listare users: </p>
-      <ListareUser numeUser="Prodan Beatrice"  label="Approved"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Rejected"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="FII"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Pending"onClick={()=> console.log("clicked - just testing again")}/>
+      <CerereListing 
+        title="Laptop Dell XPS 15" 
+        label="Accepted" 
+        location = "Facultatea de Informatica FII"
+        onClick={()=> console.log("clicked - just checking if it works")} />
 
-      <p>Listare users: </p>
-      <ListareUser numeUser="Prodan Beatrice"  label="Approved"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Rejected"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="FII"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Pending"onClick={()=> console.log("clicked - just testing again")}/>
+      <CerereListing 
+        title="Laptop Dell XPS 15" 
+        label="Rejected" 
+        location = "Facultatea de Informatica FII"
+        onClick={()=> console.log("clicked - just checking if it works")} />
+      
+
 
       <p>Listare produs: </p>
       <ListareProdus
