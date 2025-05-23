5c6ce4e9eb301dbf0792357c20d24e79684bf6f3
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-23 17:27:12 +0300

    #109-Componenta listare cerere admin

--- src/app/home/testing/listare-cerere/page.js
+++ src/app/home/testing/listare-cerere/page.js
@@ -1,33 +1,53 @@
 'use client'
 
-import CerereListing from '@/lib/components/home/echipamente/CerereListingStudent';
-import CerereListingLaborant from '@/lib/components/home/echipamente/CerereListingLaborant';
-import ListareUser from '@/lib/components/home/echipamente/ListareUser';
 import ListareProdus from '@/lib/components/home/echipamente/ProdusListing';
+import CerereListingStudent from '@/lib/components/home/echipamente/CerereListingStudent';
+import CerereListingAdmin from '@/lib/components/home/echipamente/CerereListingAdmin';
 
 export default function Page() {
   return (
     <div className="content-wrapper">
 
       <p>Cereri studenti</p>
-      <CerereListing 
+      <CerereListingStudent
         title="Laptop Dell XPS 15" 
         label="Pending" 
         location = "Facultatea de Informatica FII"
         onClick={()=> console.log("clicked - just checking if it works")} />
 
-      <CerereListing 
+      <CerereListingStudent
         title="Laptop Dell XPS 15" 
         label="Accepted" 
         location = "Facultatea de Informatica FII"
         onClick={()=> console.log("clicked - just checking if it works")} />
 
-      <CerereListing 
+      <CerereListingStudent
         title="Laptop Dell XPS 15" 
         label="Rejected" 
         location = "Facultatea de Informatica FII"
         onClick={()=> console.log("clicked - just checking if it works")} />
       
+      <p>Cereri admin</p>
+      <CerereListingAdmin
+        title="Laptop Dell XPS 15"  
+        location = "Facultatea de Informatica FII"
+        user="Prodan Beatrice"
+        label="Pending"
+        onClick={()=> console.log("clicked - just checking if it works")} />
+
+      <CerereListingAdmin
+        title="Laptop Dell XPS 15"  
+        location = "Facultatea de Informatica FII"
+        user="Prodan Beatrice"
+        label="Accepted"
+        onClick={()=> console.log("clicked - just checking if it works")} />
+
+      <CerereListingAdmin
+        title="Laptop Dell XPS 15"  
+        location = "Facultatea de Informatica FII"
+        user="Prodan Beatrice"
+        label="Rejected"
+        onClick={()=> console.log("clicked - just checking if it works")} />
 
 
       <p>Listare produs: </p>
