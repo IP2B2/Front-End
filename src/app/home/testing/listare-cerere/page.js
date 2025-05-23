6100f555cd030412ab547dd6e7b97bc3a64d5d8f
beatrice~ <prodanbeatrice5@gmail.com>
2025-05-23 16:48:42 +0300

    #108-componenta-listare-cerere-laborant

--- src/app/home/testing/listare-cerere/page.js
+++ src/app/home/testing/listare-cerere/page.js
@@ -7,20 +7,31 @@ import ListareUser from '@/lib/components/home/echipamente/ListareUser';
 export default function Page() {
   return (
     <div className="content-wrapper">
-      <p>Cereri listing studenti</p>
-      <CerereListing title="Laptop Dell XPS 15" label="Mentenanță" onClick={()=> console.log("clicked - just checking if it works")} />
-      <CerereListing title="Prelungitor cu mâner" label="Valabil"onClick={()=> console.log("clicked - just testing again")}/>
+      <p>Cereri listing laborant:</p>
 
-      <p>Cereri listing laborant: </p>
-      <CerereListingLaborant title="Prelungitor 1" studentName="Prodan Beatrice" label="Pending"onClick={()=> console.log("clicked - just testing again")}/>
-      <CerereListingLaborant title="Prelungitor 2" studentName="Prodan Beatrice" label="Accepted"onClick={()=> console.log("clicked - just testing again")}/>
-      <CerereListingLaborant title="Prelungitor 3" studentName="Prodan Beatrice" label="Rejected"onClick={()=> console.log("clicked - just testing again")}/>
+      <CerereListingLaborant
+        title="Prelungitor 1"
+        location="Facultatea de Informatica Iasi"
+        user="Prodan Beatrice"
+        label="Pending"
+        onClick={() => console.log("clicked - test 1")}
+      />
 
-      <p>Listare users: </p>
-      <ListareUser numeUser="Prodan Beatrice"  label="Approved"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Rejected"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="FII"onClick={()=> console.log("clicked - just testing again")}/>
-      <ListareUser numeUser="Prodan Beatrice"  label="Pending"onClick={()=> console.log("clicked - just testing again")}/>
+      <CerereListingLaborant
+        title="Prelungitor 2"
+        location="Facultatea de Informatica Iasi"
+        user="Prodan Beatrice"
+        label="Accepted"
+        onClick={() => console.log("clicked - test 2")}
+      />
+
+      <CerereListingLaborant
+        title="Prelungitor 3"
+        location="Facultatea de Informatica Iasi"
+        user="Prodan Beatrice"
+        label="Rejected"
+        onClick={() => console.log("clicked - test 3")}
+      />
     </div>
     
   );
