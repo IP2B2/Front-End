01d229c2cb5c9290621162e0a1e19916186ab99b
Teodor <teoddeleanu@gmail.com>
2025-05-24 11:24:14 +0300

    #106 popup component created and test page updated

--- src/app/home/testing/adaugare-produs/page.js
+++ src/app/home/testing/adaugare-produs/page.js
@@ -1,46 +1,19 @@
 "use client";
 
 import { useState } from "react";
-import styles from './productAddedSuccessfully.module.css';
-import Image from "next/image";
+import ProductAddedSucc from "@/lib/components/popups/ProductAddedSucc";
+import styles from "./page.module.css";
 
-export default function PaginaPopup() {
-  const [popupDeschis, setPopupDeschis] = useState(false);
-  /*pot face ca un click in afara popupului sa dea exit din popup/////////////////////////////////////////////////*/
+export default function Page() {
+  const [showProdAdSuc, setShowProdAdSuc] = useState(false);
+
   return (
-    <div className={styles.layout}>
-      <div className={styles.container}>
-        <h1>test popup</h1>
-        <button onClick={() => setPopupDeschis(true)}>Deschide popup</button>
+    <div className={styles.pageWrapper}>
+      <button className={styles.prodAdSucButton} onClick={() => setShowProdAdSuc(true)}>
+        Produs adăugat cu succes
+      </button>
 
-        {popupDeschis && (
-          <div className={styles.popupOverlay}>
-            <div className={styles.popupBox}>
-              <button
-                className={styles.xButton}
-                onClick={() => setPopupDeschis(false)}
-              >
-                &#10005;
-              </button>
-
-              <div className={styles.popupContent}>
-                <div className={styles.imageWrapper}>
-                  <Image src="/animated-characters/jumping.svg"
-                    alt="Character"
-                    fill
-                    style={{ objectFit: "contain" }}
-
-                  />
-                </div>
-                <h2 className={styles.successTitle}>Produs adăugat cu succes!</h2>
-                <p className={styles.successMessage}>
-                  Ai adăugat produsul cu succes! Acesta va fi disponibil imediat în lista de produse
-                </p>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
+      <ProductAddedSucc open={showProdAdSuc} onClose={() => setShowProdAdSuc(false)} />
     </div>
   );
 }
