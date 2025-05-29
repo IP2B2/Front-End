716d7da6438f1090fa1a32de11a203b9804b917e
Daniel Birleanu <65033345+eejdan@users.noreply.github.com>
2025-05-30 00:45:27 +0300

    #integrare-2 acasa init

---
+++ src/app/acasa/echipamente/page.js
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Echipamente() {
+    return (
+        <div>
+            <h1>Echipamente</h1>
+            <p>Aici vor fi afișate echipamentele tale.</p>
+            {/* Implementarea ulterioară va depinde de specificațiile aplicației */}
+        </div>
+    );
+}
