67cb9d9899adaef5dcf481a437a5b8d7fccb55a1
Daniel Birleanu <65033345+eejdan@users.noreply.github.com>
2025-05-30 14:31:08 +0300

    #INT2 fixed root page

--- src/lib/components/auth/authBannerContainer.js
+++ src/lib/components/auth/authBannerContainer.js
@@ -6,7 +6,7 @@ import { Montserrat400 } from '@/lib/fonts/Montserrat';
 import '@/app/globals.css'
 import styles from './authBannerContainer.module.css'
 
-const BannerContainer = () => {
+const BannerContainer = ({ keepWhite }) => {
     return (
     <div className={styles.bannerContainer}>
         <div className={styles.desktopWrapper}>
@@ -20,7 +20,7 @@ const BannerContainer = () => {
         </div>
         <div className={styles.tabletWrapper}>
             <Image
-                    src="/IsmaBlack.svg"
+                    src={!keepWhite ? "/IsmaBlack.svg" : "/ISMA.svg"}
                     width={400}
                     height={152}
                     alt="Project Logo"
@@ -28,7 +28,7 @@ const BannerContainer = () => {
         </div>
         <div className={styles.mobileWrapper}>
             <Image
-                    src="/IsmaBlack.svg"
+                    src={!keepWhite ? "/IsmaBlack.svg" : "/ISMA.svg"}
                     width={200}
                     height={76}
                     alt="Project Logo"
